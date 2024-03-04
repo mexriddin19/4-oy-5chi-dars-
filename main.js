9a8a035706e981f8c4253e651c547808fac6af12
mexriddin19 <gsalohiddin29@gmail.com>
2024-03-04 22:21:58 +0500

    nihao

---
+++ main.js
@@ -0,0 +1,8 @@
+let ass = alert('assalomu aleykum')
+let ism = prompt("ismingizni kiriting kiriting😌");
+let harf = prompt("bitta hohlagan harifizi kiriting");
+if (ism.includes(harf)) {
+  alert(ism + "isim ichida " + harf +' harifi bor');
+} else {
+  alert(ism + "isim ichida " + harf + " harifi yoq");
+}
